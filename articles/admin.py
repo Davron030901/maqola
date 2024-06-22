f8dff6503c9c4255887c2638ec3870cef9cd8c87
Davron <152484311+Davron030901@users.noreply.github.com>
2024-06-22 04:57:42 -0500

    Update admin.py
    a

--- articles/admin.py
+++ articles/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-<<<<<<< HEAD
 
-=======
 from .models import Maqola
 @admin.register(Maqola)
 class MaqolaAdmin(admin.ModelAdmin):
     list_display=['id','title']
     class Meta:
         model=Maqola
->>>>>>> fc755d3 (blog_site a)
-# Register your models here.
